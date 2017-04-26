479d1222bd620f3445f72ef027c5397564e7ea67
frehman <fike.rehman@gmail.com>
2017-04-25 19:44:16 -0500

    Initial commit of the Elara project

---
+++ ElaraServiceHost/Program.cs
@@ -0,0 +1,26 @@
+﻿using Topshelf;
+
+namespace CTS.Elara
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(config =>
+            {
+                config.Service<ElaraService>(svr =>
+                {
+                    svr.ConstructUsing(name => new ElaraService());
+                    svr.WhenStarted(tc => tc.Start());
+                    svr.WhenStopped(tc => tc.Stop());
+                });
+
+                config.RunAsLocalSystem();
+
+                config.SetDescription("Elara Service hosts a gateway that enables communications between Elara Arduino board and the Internet");
+                config.SetDisplayName("Elara Service");
+                config.SetServiceName("ElaraService");
+            });
+        }
+    }
+}
