3584dd227797b3217511494678d458fd377efe8e
frehman <fike.rehman@gmail.com>
2017-04-26 20:34:15 -0500

    Integrated the Log4net logger

--- ElaraServiceHost/ElaraService.cs
+++ ElaraServiceHost/ElaraService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +9,12 @@ namespace CTS.Elara
 {
     public class ElaraService
     {
+        private static readonly log4net.ILog _logger =
+                 log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
         public void Start()
         {
+            _logger.Debug("Elara Service Started my man!");
 
         }
 
--- ElaraServiceHost/Program.cs
+++ ElaraServiceHost/Program.cs
@@ -8,18 +8,21 @@ namespace CTS.Elara
         {
             HostFactory.Run(config =>
             {
+                config.UseLog4Net("log4net.config", true);
+
                 config.Service<ElaraService>(svr =>
                 {
                     svr.ConstructUsing(name => new ElaraService());
-                    svr.WhenStarted(tc => tc.Start());
-                    svr.WhenStopped(tc => tc.Stop());
+                    svr.WhenStarted(es => es.Start());
+                    svr.WhenStopped(es => es.Stop());
                 });
-
-                config.RunAsLocalSystem();
 
                 config.SetDescription("Elara Service hosts a gateway that enables communications between Elara Arduino board and the Internet");
                 config.SetDisplayName("Elara Service");
                 config.SetServiceName("ElaraService");
+
+                config.RunAsLocalSystem();
+                config.StartAutomaticallyDelayed();
             });
         }
     }
